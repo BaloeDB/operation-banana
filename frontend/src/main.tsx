475b4f4ad7aa48b75ff21417206ac35b2102c4b3
melleg <kmm.gubbels@gmail.com>
2024-02-05 11:41:44 +0100

    Brought back strictmode, renamed App to Root

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,15 @@
 import ReactDOM from "react-dom/client";
-import App from "./Root.tsx";
+import Root from "./Root.tsx";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import BananaClicker from "./routes/BananaClicker.tsx";
 import Home from "./routes/Home.tsx";
+import React from "react";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Root />,
     children: [
       {
         path: "",
@@ -23,5 +24,7 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
