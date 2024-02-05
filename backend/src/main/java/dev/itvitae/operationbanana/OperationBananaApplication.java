752bee7367fad125459dc001f8384741dbcbd418
BaloeTheBear <76252234+BaloeTheBear@users.noreply.github.com>
2024-02-05 10:33:03 +0100

    Setup

---
+++ backend/src/main/java/dev/itvitae/operationbanana/OperationBananaApplication.java
@@ -0,0 +1,13 @@
+package dev.itvitae.operationbanana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationBananaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperationBananaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/itvitae/operationbanana/OperationBananaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.itvitae.operationbanana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperationBananaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
