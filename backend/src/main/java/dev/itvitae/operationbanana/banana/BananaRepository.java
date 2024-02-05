469b5f7ecfab1400c4412a31fa856192c9216835
RaafiKuijper <raafi.kuijper@itvitaelearning.nl>
2024-02-05 11:15:57 +0100

    Add Banana/Brand repository

---
+++ backend/src/main/java/dev/itvitae/operationbanana/banana/BananaRepository.java
@@ -0,0 +1,7 @@
+package dev.itvitae.operationbanana.banana;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BananaRepository extends JpaRepository<Banana, Long> {
+
+}
---
+++ backend/src/main/java/dev/itvitae/operationbanana/brand/BrandRepository.java
@@ -0,0 +1,7 @@
+package dev.itvitae.operationbanana.brand;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BrandRepository extends JpaRepository<Brand, Long> {
+
+}
